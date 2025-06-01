88a9da05ff4b40e8da8b9c1ebcf87b99f8a62bbf
Krishna Kireeti <krishnakireeti2k7@gmail.com>
2025-06-01 08:19:27 +0530

    Hive setup

---
+++ lib/models/task.dart
@@ -0,0 +1,17 @@
+import 'package:hive/hive.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject{
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  DateTime startTime;
+
+  @HiveField(2)
+  DateTime endTime;
+
+  Task({required this.title, required this.startTime, required this.endTime});
+}
